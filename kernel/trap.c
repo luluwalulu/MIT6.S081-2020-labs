abd89efb7f0b1137b3724cb32cad7e8635f77acd
luluwalulu <553694212@qq.com>
2025-11-24 19:50:59 +0800

    lab trap:打备注

--- kernel/trap.c
+++ kernel/trap.c
@@ -103,7 +103,7 @@ usertrapret(void)
   // the process next re-enters the kernel.
   p->trapframe->kernel_satp = r_satp();         // kernel page table
   p->trapframe->kernel_sp = p->kstack + PGSIZE; // process's kernel stack
-  p->trapframe->kernel_trap = (uint64)usertrap;
+  p->trapframe->kernel_trap = (uint64)usertrap; // trampoline.S 73行
   p->trapframe->kernel_hartid = r_tp();         // hartid for cpuid()
 
   // set up the registers that trampoline.S's sret will use
