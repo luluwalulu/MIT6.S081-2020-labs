cf1ce0485d94075967da4bd6ca0beb074b58de13
luluwalulu <553694212@qq.com>
2025-12-07 15:11:54 +0800

    lab thread:uthread完成

--- user/uthread.c
+++ user/uthread.c
@@ -10,11 +10,25 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
-
 struct thread {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
@@ -41,6 +55,7 @@ thread_schedule(void)
   next_thread = 0;
   t = current_thread + 1;
   for(int i = 0; i < MAX_THREAD; i++){
+    // 如果超出范围就回到线程表的起始开始查找
     if(t >= all_thread + MAX_THREAD)
       t = all_thread;
     if(t->state == RUNNABLE) {
@@ -55,6 +70,7 @@ thread_schedule(void)
     exit(-1);
   }
 
+  // 如果查找到的下一个可运行线程不是当前线程，就运行next_thread
   if (current_thread != next_thread) {         /* switch threads?  */
     next_thread->state = RUNNING;
     t = current_thread;
@@ -63,6 +79,10 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    
+    // 保存当前线程t的状态，并转向新线程current_thread
+    thread_switch((uint64)t,(uint64)current_thread);
+    // 之后直接结束函数，返回thread_yield，并最终返回调用thread_yield的函数即可
   } else
     next_thread = 0;
 }
@@ -77,6 +97,22 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+
+  // 设定各寄存器的值
+  t->ra=(uint64)func;
+  t->sp=(uint64)(t->stack + STACK_SIZE);
+  t->s0=0;
+  t->s1=0;
+  t->s2=0;
+  t->s3=0;
+  t->s4=0;
+  t->s5=0;
+  t->s6=0;
+  t->s7=0;
+  t->s8=0;
+  t->s9=0;
+  t->s10=0;
+  t->s11=0;
 }
 
 void 
@@ -158,6 +194,7 @@ main(int argc, char *argv[])
   thread_create(thread_a);
   thread_create(thread_b);
   thread_create(thread_c);
+  // 在执行完这个thread_schedule后，就永远不会回到main线程
   thread_schedule();
   exit(0);
 }
